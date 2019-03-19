14ba710a25ca1a6368a95b7a20e5418c7a361bb6
tfquirk <timothyfquirk@gmail.com>
2019-03-19 12:01:37 -0400

    updated HTML for bet form

--- src/components/Bets/BetsForm.js
+++ src/components/Bets/BetsForm.js
@@ -1,77 +1,88 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { connect } from "react-redux";
 //import { signup } from "../../actions/fetchActions";
 
 import "../../styles/Signup.css";
 
-const BetsForm = props => {
-  const handleSubmit = () => {
+class BetsForm extends Component {
+  state = {
+    betInfo: this.props.betInfo,
+    amount: 0,
+    winner: ""
+  };
+
+  handleSubmit = () => {
     console.log("submitted");
   };
-  console.log("BETSFORM", props);
-  // return signup form HTML
-  if (props.betInfo !== {}) {
-    return (
-      <div className="signupContainer">
-        <div className="signupForm">
-          <form onSubmit={handleSubmit}>
-            <h1>Make A Bet</h1>
-            <p>
-              Please select your wager amount and the outcome you are betting
-              on.
-            </p>
-            <hr />
-            <button type="button" value="50" name="add50">
-              $50
-            </button>
-            <button type="button" value="100" name="add100">
-              $100
-            </button>
-            <button type="button" value="250" name="add250">
-              $250
-            </button>
-            <button type="button" value="500" name="add500">
-              $500
-            </button>
-            <label>$</label>
-            <input type="text" name="addValue" placeholder="Enter Amount" />
-            <br />
-            <br />
-            <input type="checkbox" name="homeWin" value="Bike" />{" "}
-            {props.betInfo.meta.homeTeam.name} win
-            <br />
-            <input type="checkbox" name="draw" value="Car" /> Draw
-            <br />
-            <input type="checkbox" name="awayWin" value="Bike" />{" "}
-            {props.betInfo.meta.awayTeam.name} win
-            <br />
-            <input type="checkbox" name="homeDraw" value="Bike" />{" "}
-            {props.betInfo.meta.homeTeam.name} or draw
-            <br />
-            <input type="checkbox" name="awayDraw" value="Bike" />{" "}
-            {props.betInfo.meta.awayTeam.name} or draw
-            <br />
-            <p>
-              By creating an account you agree to our{" "}
-              <a href="/betterSportsTerms">Terms & Privacy</a>.
-            </p>
-            <div className="clearfix">
-              <button type="button" className="cancelbtn">
-                Cancel
+
+  render() {
+    console.log(this.state.checked);
+    if (this.props.betInfo !== {}) {
+      return (
+        <div className="signupContainer">
+          <div className="signupForm">
+            <form onSubmit={this.handleSubmit}>
+              <h1>Make A Bet</h1>
+              <p>
+                Please select your wager amount and the outcome you are betting
+                on.
+              </p>
+              <hr />
+              <button type="button" value="50" name="amount">
+                $50
               </button>
-              <button type="submit" className="signupbtn">
-                Submit
+              <button type="button" value="100" name="amount">
+                $100
               </button>
-            </div>
-          </form>
+              <button type="button" value="250" name="amount">
+                $250
+              </button>
+              <button type="button" value="500" name="amount">
+                $500
+              </button>
+              <label>$</label>
+              <input type="text" name="addValue" placeholder="Enter Amount" />
+              <br />
+              <br />
+              <button type="button" value="50" name="amount">
+                {this.state.betInfo.meta.homeTeam.name} win
+              </button>
+              <button type="button" value="100" name="amount">
+                Draw
+              </button>
+              <button type="button" value="250" name="amount">
+                {this.state.betInfo.meta.awayTeam.name} win
+              </button>
+              <button type="button" value="500" name="amount">
+                {this.state.betInfo.meta.homeTeam.name} or draw
+              </button>
+              <button type="button" value="500" name="amount">
+                {this.state.betInfo.meta.awayTeam.name} or draw
+              </button>
+
+              <br />
+              <p>
+                By creating an account you agree to our{" "}
+                <a href="/betterSportsTerms">Terms & Privacy</a>.
+              </p>
+              <div className="clearfix">
+                <button type="button" className="cancelbtn">
+                  Cancel
+                </button>
+                <button type="submit" className="signupbtn">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
-    );
-  } else {
-    return <h1>Loading...</h1>;
+      );
+    } else {
+      return <h1>Loading...</h1>;
+    }
   }
-};
+}
 
 const mapStateToProps = state => {
   return {
