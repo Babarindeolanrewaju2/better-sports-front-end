bb03e63fd99f406f93e69475b551cdac4d3abd97
tfquirk <timothyfquirk@gmail.com>
2019-03-22 14:46:52 -0400

    updated soccer teams index page css

--- src/components/Teams/Soccer/SoccerTeamsIndex/SoccerTeamCard.js
+++ src/components/Teams/Soccer/SoccerTeamsIndex/SoccerTeamCard.js
@@ -1,19 +1,21 @@
 import React from "react";
 
-//import { connect } from "react-redux";
-
 const SoccerTeamCard = props => {
+  // grab team attributes from props
   let team = props.team.attributes;
+
+  // on user click, push to that team's show page based on team id
   const handleClick = () => {
     props.routerProps.history.push(`/teams/soccer/${team.id}`);
   };
+
   return (
-    <div className="SoccerTeamCard" onClick={handleClick}>
-      <div className="flip-card-inner">
-        <div className="flip-card-front">
+    <div className="soccerTeamCard" onClick={handleClick}>
+      <div className="soccer-card-inner">
+        <div className="soccer-card-front">
           <img src={team.team_logo} alt="Avatar" />
         </div>
-        <div className="flip-card-back">
+        <div className="soccer-card-back">
           <h1>{team.name}</h1>
           <h3>{team.league}</h3>
         </div>
