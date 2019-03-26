b11dfd200ef756f492d3c6864d716e0452b940b4
tfquirk <timothyfquirk@gmail.com>
2019-03-26 13:55:57 -0400

    PlayerShow initial css created

--- src/components/Teams/Soccer/SoccerPlayerShow/PlayerShowPage.js
+++ src/components/Teams/Soccer/SoccerPlayerShow/PlayerShowPage.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import { fetchPlayerInfo } from "../../../../actions/soccerTeamsActions";
 
-import "../../../../styles/SoccerTeam.css";
+import "../../../../styles/PlayerShow.css";
 
 class PlayerShowPage extends Component {
   state = {
@@ -23,42 +23,46 @@ class PlayerShowPage extends Component {
 
     if (player) {
       return (
-        <div className="PlayerShowContainer">
-          <div className="PlayerShowImage">
-            <img
-              src={
-                player.profile_image ||
-                "https://www.minithrowballs.com/images/clipart/sports/Soccer/scab014.svg"
-              }
-              alt="Player"
-            />
-          </div>
+        <div className="playerShowContainer">
+          <div className="playerInnerContainer">
+            <div className="playerPrimaryImageAndInfo">
+              <div className="playerShowImage">
+                <img
+                  src={
+                    player.profile_image ||
+                    "https://www.minithrowballs.com/images/clipart/sports/Soccer/scab014.svg"
+                  }
+                  alt="Player"
+                />
+              </div>
 
-          <div className="PlayerShowPrimaryInfo">
-            <h1>{player.full_name}</h1>
+              <div className="playerShowPrimaryInfo">
+                <h1>{player.full_name}</h1>
 
-            <h3>Team: {player.team_name}</h3>
-            <h3>Signed: {player.date_signed}</h3>
-            <h3>Nationality: {player.nationality}</h3>
-          </div>
+                <h3>Team: {player.team_name}</h3>
+                <h3>Signed: {player.date_signed}</h3>
+                <h3>Nationality: {player.nationality}</h3>
+              </div>
+            </div>
 
-          <div className="PlayerShowDescription">
-            <h2>Details:</h2>
+            <div className="playerShowDescription">
+              <h2>Details:</h2>
 
-            <h4>{player.birth_location && `Born: ${player.birth_location}`}</h4>
-            <h4>
-              {player.date_of_birth && `Birthday: ${player.date_of_birth}`}
-            </h4>
-            <h4>
-              {player.contracted_salary &&
-                `Contracted Salary: ${player.contracted_salary}`}
-            </h4>
-            <h4>{player.height && `Height: ${player.height}`} </h4>
+              <h4>
+                {player.birth_location && `Born: ${player.birth_location}`}
+              </h4>
+              <h4>
+                {player.date_of_birth && `Birthday: ${player.date_of_birth}`}
+              </h4>
+              <h4>
+                {player.contracted_salary &&
+                  `Contracted Salary: ${player.contracted_salary}`}
+              </h4>
+              <h4>{player.height && `Height: ${player.height}`} </h4>
 
-            <p>
-              Description: <br />
-              {player.description}
-            </p>
+              <h2>Description:</h2>
+              <p>{player.description}</p>
+            </div>
           </div>
         </div>
       );
