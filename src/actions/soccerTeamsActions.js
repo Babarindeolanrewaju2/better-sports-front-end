b1a509c5c8b68c27bbb44d07a68cee2e369a1e43
tfquirk <timothyfquirk@gmail.com>
2019-03-26 10:02:18 -0400

    Added GET requested for specific player info to soccerTeamActions

--- src/actions/soccerTeamsActions.js
+++ src/actions/soccerTeamsActions.js
@@ -1,7 +1,8 @@
 const TEAMS_API = "http://localhost:8000/api/v1/teams";
 const TEAM_PLAYERS = "http://localhost:8000/api/v1/team_players";
+const PLAYER = "http://localhost:8000/api/v1/players/";
 
-// fetches all soccer teams from API and updates state to include them
+// fetches all soccer teams from API and updates Redux state to include them
 export function fetchTeams(dispatch) {
   return fetch(TEAMS_API)
     .then(res => res.json())
@@ -10,13 +11,27 @@ export function fetchTeams(dispatch) {
     });
 }
 
-//
+// Hits the API endpoint to fetch all players for a particular team based on
+// on that team ID.
+// This function is used in the SoccerTeamShow component
 export function fetchTeamPlayers(team_id) {
   return fetch(TEAM_PLAYERS, {
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
       teamId: team_id
+    }
+  }).then(res => res.json());
+}
+
+// Looks up a specific player from the API based on the players ID
+// This function is used in the PlayerShowPage component
+export function fetchPlayerInfo(player_id) {
+  return fetch(PLAYER + player_id, {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      playerId: player_id
     }
   }).then(res => res.json());
 }
