f30c1cc6d4b51f04f869cb353a85f3a54c204c3e
tfquirk <timothyfquirk@gmail.com>
2019-03-29 11:29:50 -0400

    created past and future games components and structured them to display the appropriate info

---
+++ src/components/Games/FutureGames.js
@@ -0,0 +1,46 @@
+import React, { Fragment } from "react";
+
+import Match from "./Match";
+
+import "../../styles/Games.css";
+
+const FutureGames = props => {
+  // create all match rows for the table to be displayed
+  const mapGameInfoToMatchCard = () => {
+    return props.futureGames.map(game => {
+      return <Match key={game.id} game={game} {...props} />;
+    });
+  };
+
+  return (
+    <div className="gamesContainer">
+      <h1>Future Games</h1>
+      <div className="gamesList">
+        {props.futureGames.length === 0 ? (
+          <h1>Loading</h1>
+        ) : (
+          <Fragment>
+            <table>
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Home Team</th>
+                  <th>Away Team</th>
+                  <th>Home Win</th>
+                  <th>Draw</th>
+                  <th>Away Win</th>
+                  <th>Home Win or Draw</th>
+                  <th>Away Win or Draw</th>
+                  <th>Bet on this game</th>
+                </tr>
+              </thead>
+              <tbody>{mapGameInfoToMatchCard()}</tbody>
+            </table>
+          </Fragment>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default FutureGames;
--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -2,14 +2,17 @@ import React, { Component, Fragment } from "react";
 
 // connect to Redux state
 import { connect } from "react-redux";
-
 // import function to get all games from the API
 import { fetchGames } from "../../actions/fetchActions";
-
-// this component returns table rows with all of the game/match information
-import Match from "./Match";
+//import FutureGames and PastGames components to display on page for users to review
+import FutureGames from "./FutureGames";
+import PastGames from "./PastGames";
 
 import "../../styles/Games.css";
+
+// create variable to hold today's date for comparison for the games returned
+// from the games API -- this comparision will help find past and future games
+const today = new Date();
 
 class Games extends Component {
   componentDidMount() {
@@ -17,38 +20,32 @@ class Games extends Component {
     this.props.getGames();
   }
 
-  // create all match rows for the table to be displayed
-  mapGameInfoToMatchCard = () => {
-    return this.props.games.map(game => {
-      return <Match key={game.id} game={game} {...this.props} />;
+  // seperate previous games, which will have a game
+  // outcome and no longer be eligible for bets
+  getPastGames = () => {
+    return this.props.games.filter(game => {
+      return new Date(game.attributes.match_date) < today;
+    });
+  };
+
+  // seperate future games, which will have a game
+  // outcome and no longer be eligible for bets
+  getFutureGames = () => {
+    return this.props.games.filter(game => {
+      return new Date(game.attributes.match_date) > today;
     });
   };
 
   render() {
     return (
       <div className="gamesContainer">
-        <h1>Future Games</h1>
         <div className="gamesList">
           {this.props.games.length === 0 ? (
             <h1>Loading</h1>
           ) : (
             <Fragment>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Date</th>
-                    <th>Home Team</th>
-                    <th>Away Team</th>
-                    <th>Home Win</th>
-                    <th>Draw</th>
-                    <th>Away Win</th>
-                    <th>Home Win or Draw</th>
-                    <th>Away Win or Draw</th>
-                    <th>Bet on this game</th>
-                  </tr>
-                </thead>
-                <tbody>{this.mapGameInfoToMatchCard()}</tbody>
-              </table>
+              <FutureGames futureGames={this.getFutureGames()} />
+              <PastGames pastGames={this.getPastGames()} />
             </Fragment>
           )}
         </div>
